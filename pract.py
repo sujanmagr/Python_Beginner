9ba023a24b0baa39d5f301532286e8f32cfef9b9
SachinMagar <budhathokisachin143@gmail.com>
2026-09-25 15:34:08 +0545

    file added

---
+++ pract.py
@@ -0,0 +1,7 @@
+print("Hello this is my ")
+print("Hello this is my ")
+print("Hello this is my ")
+print("Hello this is my ")
+print("Hello this is my ")
+print("Hello this is my ")
+print("Hello this is my ")
